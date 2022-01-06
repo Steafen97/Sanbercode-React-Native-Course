ca94b8353e976c3be49ceeaf15b3f10cfc4f3caf
Steafen97 <steafenk@gmail.com>
2022-01-06 18:45:22 +0700

    Tugas 4

---
+++ Tugas-4-Function/function.js
@@ -0,0 +1,30 @@
+//Soal No 1
+
+function teriak() {
+    return "Halo Sanbers!";
+}
+console.log(teriak())
+
+//Soal No 2
+
+function hasilKali(num1,num2){
+    return num1*num2;
+}
+
+var num1 = 12;
+var num2 = 4;
+
+console.log(hasilKali(num1,num2))
+
+//Soal No 3
+
+function perkenalan(nama,age,address,hobby) {
+    return ("Nama saya "+nama+", umur saya "+String(age)+", alamat saya di "+address+", dan saya punya hobby yaitu "+hobby+"!");
+}
+
+var nama = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogyakarta";
+var hobby = "Gaming";
+
+console.log(perkenalan(nama,age,address,hobby))
