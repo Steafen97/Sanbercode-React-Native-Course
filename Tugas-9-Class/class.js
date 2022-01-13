480b90e5c91579cbc2c8f6d4a8041043671075a5
Steafen97 <steafenk@gmail.com>
2022-01-13 18:43:12 +0700

    Tugas 9

---
+++ Tugas-9-Class/class.js
@@ -0,0 +1,103 @@
+// Soal No 1
+
+//Release 0
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    get legs() {
+        return 4;
+    }
+    get cold_blooded() {
+        return false;
+    }
+}
+ 
+const sheep = new Animal("shaun");
+ 
+console.log(sheep.name)
+console.log(sheep.legs) 
+console.log(sheep.cold_blooded)
+
+//Release 1
+class Ape extends Animal{
+    constructor(name){
+        super(name);
+    }
+    get legs() {
+        return 2;
+    }
+    get cold_blooded(){
+        return false;
+    }
+    yell() {
+        console.log("Auooo");
+    };
+}
+
+const sungokong = new Ape("kera sakti");
+console.log(sungokong.name); // "kera sakti"
+console.log(sungokong.legs); // 2
+console.log(sungokong.cold_blooded); // false
+sungokong.yell(); // "Auooo"
+
+class Frog extends Animal{
+    constructor(name){
+        super(name);
+    }
+    get legs() {
+        return 4;
+    }
+    get cold_blooded(){
+        return true;
+    }
+    jump() {
+        console.log("hop hop");
+    };
+}
+
+const kodok = new Frog("buduk");
+console.log(kodok.name); // "buduk"
+console.log(kodok.legs); // 4
+console.log(kodok.cold_blooded); // true
+kodok.jump(); // "hop hop"
+
+// Soal No 2
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+var clock = new Clock({template: 'h:m:s'});
+clock.start(); 
