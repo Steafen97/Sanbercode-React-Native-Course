9c822674873f87067ae33b9493a991ffd01a723e
Steafen97 <steafenk@gmail.com>
2022-01-18 22:41:09 +0700

    Tugas 12

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,19 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Telegram from './Tugas/Tugas-12/Telegram';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Telegram/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ SanberApp/Tugas/Tugas-12/dummy.js
@@ -0,0 +1,44 @@
+const Data = [
+	{
+		id: "1",
+		image: require("./asset/profile1.png"),
+		name: "Achmad Hilmy",
+		message: "Main Dota kapan nih?",
+		time: "19.16",
+		totalMessage: "2",
+	},
+	{
+		id: "2",
+		image: require("./asset/profile2.png"),
+		name: "Deddy Corbuzier",
+		message: "Gym bareng ya besok jam 18.00",
+		time: "18.25",
+		totalMessage: "4",
+	},
+	{
+		id: "3",
+		image: require("./asset/profile3.png"),
+		name: "Deny Manusia Ikan",
+		message: "Sini bro,gw lagi masak ikan",
+		time: "17.00",
+		totalMessage: "1",
+	},
+	{
+		id: "4",
+		image: require("./asset/profile4.png"),
+		name: "Jess No Limit",
+		message: "Content buat bulan depan apa ya enaknya?",
+		time: "15.04",
+		totalMessage: "2",
+	},
+	{
+		id: "5",
+		image: require("./asset/profile5.png"),
+		name: "Jerome Polin",
+		message: "Yuk belajar Matematika!",
+		time: "12.46",
+		totalMessage: "4",
+	}	
+];
+
+export { Data };
