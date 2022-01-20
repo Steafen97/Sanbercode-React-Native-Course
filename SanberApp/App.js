38e5bba85e8115e4ef9d4c91e3c9ce83f1b748c6
Steafen97 <steafenk@gmail.com>
2022-01-20 23:01:45 +0700

    Tugas 14

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Telegram from './Tugas/Tugas-12/Telegram';
+import RestApi from './Tugas/Tugas-14/RestApi';
 
 export default function App() {
   return (
-    <Telegram/>
+    //<Telegram/>
+    <RestApi/>
   );
 }
 
