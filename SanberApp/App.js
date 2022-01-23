5e1e6474432c29c1746bbd6882287dce3b085057
Steafen97 <steafenk@gmail.com>
2022-01-23 11:29:01 +0700

    Quiz 3

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Telegram from './Tugas/Tugas-12/Telegram';
 import RestApi from './Tugas/Tugas-14/RestApi';
 import Tugas15 from './Tugas/Tugas-15/index';
+import Quiz3 from './Quiz-3/index';
 
 export default function App() {
   return (
     //<Telegram/>
     //<RestApi/>
-    <Tugas15/>
+    //<Tugas15/>
+    <Quiz3/>
   );
 }
 
